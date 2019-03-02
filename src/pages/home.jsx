b6ba194f520ba960db0eace2b112fc906e8c40d3
ask <askbk@protonmail.com>
2019-03-02 09:14:02 +0100

    Refaktorerte Home til en klasse

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -17,35 +17,43 @@ import {
   Button
 } from 'framework7-react';
 
-export default () => (
-  <Page name="home">
-    {/* Top Navbar */}
-    <Navbar sliding={false}>
-      <NavTitle sliding>InMarket</NavTitle>
-      <NavRight>
-        <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="right" />
-      </NavRight>
-    </Navbar>
-    {/* Toolbar */}
-    <Toolbar bottom>
-      <Link>Left Link</Link>
-      <Link>Right Link</Link>
-    </Toolbar>
-    {/* Page content */}
-    <Block strong>
-      <p>Here is your blank Framework7 app.</p>
-    </Block>
+export default class extends React.Component {
+    constructor() {
+        super();
+    }
 
-    <BlockTitle>Modals</BlockTitle>
-    <Block strong>
-      <Row>
-        <Col width="50">
-          <Button fill raised popupOpen="#my-popup">Popup</Button>
-        </Col>
-        <Col width="50">
-          <Button fill raised loginScreenOpen="#my-login-screen">Login Screen</Button>
-        </Col>
-      </Row>
-    </Block>
-  </Page>
-);
+    render() {
+        return (
+            <Page name="home">
+              {/* Top Navbar */}
+              <Navbar sliding={false}>
+                <NavTitle sliding>InMarket</NavTitle>
+                <NavRight>
+                  <Link iconIos="f7:menu" iconMd="material:menu" panelOpen="right" />
+                </NavRight>
+              </Navbar>
+              {/* Toolbar */}
+              <Toolbar bottom>
+                <Link>Left Link</Link>
+                <Link>Right Link</Link>
+              </Toolbar>
+              {/* Page content */}
+              <Block strong>
+                <p>Here is your blank Framework7 app.</p>
+              </Block>
+
+              <BlockTitle>Modals</BlockTitle>
+              <Block strong>
+                <Row>
+                  <Col width="50">
+                    <Button fill raised popupOpen="#my-popup">Popup</Button>
+                  </Col>
+                  <Col width="50">
+                    <Button fill raised loginScreenOpen="#my-login-screen">Login Screen</Button>
+                  </Col>
+                </Row>
+              </Block>
+            </Page>
+        )
+    }
+}
