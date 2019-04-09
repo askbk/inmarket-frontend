b6e966cbde1de2c1dfbb0fb7ba2070b6854e992d
Filip Tangen <filiptangen1998@live.com>
2019-04-09 13:34:07 +0200

    Lager date picker

--- src/pages/employee.jsx
+++ src/pages/employee.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+
+import ReactDOM from 'react-dom';
+import DatePicker from 'react-mobile-datepicker';
 //import components
 import EmployeeHeader from '../components/EmployeePage/EmployeeHeader/EmployeeHeader.jsx';
 import Button from '../components/shared/Button/StyledButton';
 import InformationBox from '../components/shared/InformationBox/InformationBox';
-import DatePicker from '../components/shared/DatePicker/DatePicker';
+/*import DatePicker from '../components/shared/DatePicker/DatePicker';*/
 import Header from '../components/Header/Header.jsx'
 
 //import picture
@@ -22,6 +25,59 @@ import {
   Toolbar,
   Link
 } from 'framework7-react';
+/*
+const monthMap = {
+  '1': 'Jan',
+  '2': 'Feb',
+  '3': 'Mar',
+  '4': 'Apr',
+  '5': 'May',
+  '6': 'Jun',
+  '7': 'Jul',
+  '8': 'Aug',
+  '9': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
+};*/
+const monthMap = {
+  '1': 'Jan',
+  '2': 'Feb',
+  '3': 'Mar',
+  '4': 'Apr',
+  '5': 'Mai',
+  '6': 'Jun',
+  '7': 'Jul',
+  '8': 'Aug',
+  '9': 'Sep',
+  '10': 'Okt',
+  '11': 'Nov',
+  '12': 'Dec',
+};
+
+const monthMapFull = {
+  '1': 'januar',
+  '2': 'februar',
+  '3': 'mars',
+  '4': 'april',
+  '5': 'mai',
+  '6': 'juni',
+  '7': 'july',
+  '8': 'august',
+  '9': 'september',
+  '10': 'oktober',
+  '11': 'november',
+  '12': 'desember',
+};
+
+
+function dateFormat(date){
+  let year = date.getFullYear();
+  if(year === new Date().getFullYear()){
+    year = "";
+  }
+  return ""  + date.getDate() + "." +  monthMapFull[(parseInt(date.getMonth())+1)] + " " + year + " kl " + date.getHours() + ":" + date.getMinutes();
+}
 
 class Employee extends Component {
   constructor(){
@@ -31,8 +87,31 @@ class Employee extends Component {
       position: "Daglig Leder, InMarket AS",
       name: "Chris Africa, 34",
       showDate: false,
+      startDate: null,
+      endDate: null,
+      isOpen: false,
     };
   }
+
+
+  handleClick () {
+    /*Datepickeren har lavere z-index enn det framework-7 bruker. så vi må tvinge den til å bli høyere*/
+    let datepickers = document.getElementsByClassName("datepicker-modal");
+    for (let i = 0; i < datepickers.length; i++) {
+      datepickers[i].style.zIndex = "100000";
+    }
+    this.setState({ isOpen: true });
+  }
+
+  handleCancel ()  {
+    this.setState({ isOpen: false });
+  }
+
+  handleSelect (time)  {
+    this.setState({ startDate:time, isOpen: false });
+  }
+
+
   render() {
     return (
     <Page name="home" className="employeePage">
@@ -48,9 +127,82 @@ class Employee extends Component {
       <Button>SE LOGG</Button>
       <Button clicked={ () => this.setState({showDate: !this.state.showDate})}>FORESPØR</Button>
     </Row>
-    <Row className="datePickerContainer">
-      {this.state.showDate ? <DatePicker /> : null}
-    </Row>
+
+      <Row className="dateButtonContainer margin">
+
+        <h3>Velg når det skal starte</h3>
+
+        <Button clicked={this.handleClick.bind(this)} style={{ backgroundColor: 'black',
+          color: 'white',
+          border: '1px solid #c08d42',
+          marginBottom: '10px',
+          minWidth: 'fit-content',
+          maxWidth: '100px',}}><span className="dateContent">{this.state.startDate ? dateFormat(this.state.startDate) : "Velg dato her"}</span></Button>
+
+        <h3>Velg når det skal slutte</h3>
+
+        <Button clicked={this.handleClick.bind(this)} style={{ backgroundColor: 'black',
+          color: 'white',
+          border: '1px solid #c08d42',
+          marginBottom: '10px',
+          minWidth: 'fit-content',
+          maxWidth: '100px',}}><span className="dateContent">{this.state.startDate ? dateFormat(this.state.startDate) : "Velg dato her"}</span></Button>
+
+      </Row>
+
+
+      <Row>
+        <a
+            className="select-btn"
+            onClick={this.handleClick.bind(this)}>
+          select time
+        </a>
+
+
+
+
+        <DatePicker
+            value={this.state.startDate ? this.state.startDate : new Date()}
+            headerFormat={"Når det tarter: DD/MM/YYYY hh:mm"}
+            showCaption={true}
+            isOpen={this.state.isOpen}
+            onSelect={(time) =>{this.handleSelect(time).bind(this)}}
+            onCancel={()=>{this.handleCancel}}
+            confirmText={"Gå til neste"}
+            cancelText={"stop"}
+            theme={"android-dark"}
+            dateConfig={{
+              'date': {
+                format: 'D',
+                caption: 'Dag',
+                step: 1,
+              },
+              'month': {
+                format: value => monthMap[value.getMonth() + 1],
+                caption: 'Måned',
+                step: 1,
+              },
+              'year': {
+                format: 'YYYY',
+                caption: 'År',
+                step: 1,
+              },
+
+              'hour': {
+                format: 'hh',
+                caption: 'Time',
+                step: 1,
+              },
+              'minute': {
+                format: 'mm',
+                caption: 'Min',
+                step: 1,
+              },
+            }}/>
+
+        <DatePicker
+        />
+      </Row>
     <InformationBox>
       Lorem Ipsum
     </InformationBox>
