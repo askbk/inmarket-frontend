6815c69c643f3ec226f7db4280462e3dbb9ffbb6
Filip Tangen <filiptangen1998@live.com>
2019-04-30 12:39:24 +0200

    Prettified

--- src/components/shared/DatePicker/DatePicker.jsx
+++ src/components/shared/DatePicker/DatePicker.jsx
@@ -1,91 +1,101 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
 
 //import styling
-import './DatePicker.css';
+import "./DatePicker.css";
 
 //import picture
-import Location from '../../../../assets-src/DatePicker/Location.png';
-
+import Location from "../../../../assets-src/DatePicker/Location.png";
 
 //Import datepicker
-import DatePickerCustomHeader from './DatePickerCustomHeader'
-import DatePickerReact from 'react-mobile-datepicker';
+import DatePickerCustomHeader from "./DatePickerCustomHeader";
+import DatePickerReact from "react-mobile-datepicker";
 
 //import Framework7 grid-components
-import {
-  Block,
-  Row,
-  Col,
-    Button
-} from 'framework7-react';
+import { Block, Row, Col, Button } from "framework7-react";
 const monthMap = {
-  '1': 'Jan',
-  '2': 'Feb',
-  '3': 'Mar',
-  '4': 'Apr',
-  '5': 'Mai',
-  '6': 'Jun',
-  '7': 'Jul',
-  '8': 'Aug',
-  '9': 'Sep',
-  '10': 'Okt',
-  '11': 'Nov',
-  '12': 'Dec',
+  "1": "Jan",
+  "2": "Feb",
+  "3": "Mar",
+  "4": "Apr",
+  "5": "Mai",
+  "6": "Jun",
+  "7": "Jul",
+  "8": "Aug",
+  "9": "Sep",
+  "10": "Okt",
+  "11": "Nov",
+  "12": "Dec"
 };
 
 const monthMapFull = {
-  '1': 'januar',
-  '2': 'februar',
-  '3': 'mars',
-  '4': 'april',
-  '5': 'mai',
-  '6': 'juni',
-  '7': 'july',
-  '8': 'august',
-  '9': 'september',
-  '10': 'oktober',
-  '11': 'november',
-  '12': 'desember',
+  "1": "januar",
+  "2": "februar",
+  "3": "mars",
+  "4": "april",
+  "5": "mai",
+  "6": "juni",
+  "7": "july",
+  "8": "august",
+  "9": "september",
+  "10": "oktober",
+  "11": "november",
+  "12": "desember"
 };
 
-function IntervalsWith5(minutes){
-  return Math.ceil(minutes/5)*5;
+function IntervalsWith5(minutes) {
+  return Math.ceil(minutes / 5) * 5;
 }
-function getMinutes(date){
+function getMinutes(date) {
   let minutes = date.getMinutes();
-  if(parseInt(minutes) < 10){
+  if (parseInt(minutes) < 10) {
     minutes = "0" + minutes;
   }
   return minutes;
 }
 
-function getHours(date){
+function getHours(date) {
   let minutes = date.getHours();
-  if(parseInt(minutes) < 10){
+  if (parseInt(minutes) < 10) {
     minutes = "0" + minutes;
   }
   return minutes;
 }
 
-function dateFormat(date){
+function dateFormat(date) {
   let year = date.getFullYear();
-  if(year === new Date().getFullYear()){
+  if (year === new Date().getFullYear()) {
     year = "";
   }
-  return " "  + date.getDate() + ". " +  monthMapFull[(parseInt(date.getMonth())+1)] + " " + year + " kl " + getHours(date) + ":" + getMinutes(date);
+  return (
+    " " +
+    date.getDate() +
+    ". " +
+    monthMapFull[parseInt(date.getMonth()) + 1] +
+    " " +
+    year +
+    " kl " +
+    getHours(date) +
+    ":" +
+    getMinutes(date)
+  );
 }
 
-function dateFormatFromAndTo(from,to){
+function dateFormatFromAndTo(from, to) {
   let fromString = dateFormat(from);
   let toString = "";
-  if(from.getFullYear() === to.getFullYear() && from.getMonth() === to.getMonth() && from.getDate() === to.getDate()){
+  if (
+    from.getFullYear() === to.getFullYear() &&
+    from.getMonth() === to.getMonth() &&
+    from.getDate() === to.getDate()
+  ) {
     toString = "-" + getHours(to) + ":" + getMinutes(to);
-    if(from.getHours() === to.getHours() && from.getMinutes() === to.getMinutes()){
+    if (
+      from.getHours() === to.getHours() &&
+      from.getMinutes() === to.getMinutes()
+    ) {
       toString = "";
     }
-  }
-  else {
+  } else {
     toString = " til ";
     toString += dateFormat(to);
   }
@@ -93,19 +103,18 @@ function dateFormatFromAndTo(from,to){
 }
 
 class DatePicker extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       startDate: null,
       endDate: null,
       selectedDate: 0,
       isOpen: false,
-      value : new Date(),
+      value: new Date()
     };
   }
 
-
-  handleClick () {
+  handleClick() {
     /*Datepickeren har lavere z-index enn det framework-7 bruker. så vi må tvinge den til å bli høyere*/
     let datepickers = document.getElementsByClassName("datepicker-modal");
     for (let i = 0; i < datepickers.length; i++) {
@@ -117,101 +126,137 @@ class DatePicker extends Component {
     now.setMinutes(IntervalsWith5(now.getMinutes()));
     value = this.state.startDate ? this.state.startDate : now;
 
-
-
-    this.setState({ isOpen: true, selectedDate:1, value:value });
+    this.setState({ isOpen: true, selectedDate: 1, value: value });
   }
 
-  handleCancel ()  {
-    this.setState({ isOpen: false ,selectedDate:0});
+  handleCancel() {
+    this.setState({ isOpen: false, selectedDate: 0 });
   }
 
-  handleSelect (time)  {
-    if(this.state.selectedDate === 1){
+  handleSelect(time) {
+    if (this.state.selectedDate === 1) {
       let endDate = this.state.endDate;
-      if(this.state.endDate === null){
-        if(endDate === null){
+      if (this.state.endDate === null) {
+        if (endDate === null) {
           endDate = time;
         }
       }
-      this.setState({ startDate:time, endDate:endDate, value:time, isOpen: true, selectedDate:2});
-
-
-    }
-    else if(this.state.selectedDate === 2){
-      this.setState({ endDate:time, isOpen: false,selectedDate:0 });
+      this.setState({
+        startDate: time,
+        endDate: endDate,
+        value: time,
+        isOpen: true,
+        selectedDate: 2
+      });
+    } else if (this.state.selectedDate === 2) {
+      this.setState({ endDate: time, isOpen: false, selectedDate: 0 });
     }
 
     this.props.onChange(this.state.startDate, this.state.endDate);
   }
 
-  renderFromDate(dateConfig){
-    return (<DatePickerReact
+  renderFromDate(dateConfig) {
+    return (
+      <DatePickerReact
         value={this.state.value}
         showCaption={true}
         isOpen={this.state.selectedDate === 1}
-        onSelect={(time) =>{this.handleSelect(time)}}
-        onCancel={()=>{this.handleCancel()}}
-        customHeader={<DatePickerCustomHeader startDate={this.state.startDate ? this.state.startDate : new Date()} endDate={this.state.endDate ? this.state.endDate : new Date()} selected ={this.state.selectedDate}/>}
+        onSelect={time => {
+          this.handleSelect(time);
+        }}
+        onCancel={() => {
+          this.handleCancel();
+        }}
+        customHeader={
+          <DatePickerCustomHeader
+            startDate={this.state.startDate ? this.state.startDate : new Date()}
+            endDate={this.state.endDate ? this.state.endDate : new Date()}
+            selected={this.state.selectedDate}
+          />
+        }
         confirmText={"Gå til neste"}
         cancelText={"Avbryt"}
         theme={"dark"}
-        dateConfig={dateConfig}/>);
-
+        dateConfig={dateConfig}
+      />
+    );
   }
-  renderToDate(dateConfig){
-    return (<DatePickerReact
+  renderToDate(dateConfig) {
+    return (
+      <DatePickerReact
         value={this.state.value}
         showCaption={true}
         isOpen={this.state.selectedDate === 2}
-        onSelect={(time) =>{this.handleSelect(time)}}
-        onCancel={()=>{this.handleCancel()}}
-        customHeader={<DatePickerCustomHeader startDate={this.state.startDate ? this.state.startDate : new Date()} endDate={this.state.endDate ? this.state.endDate : new Date()} selected ={this.state.selectedDate}/>}
+        onSelect={time => {
+          this.handleSelect(time);
+        }}
+        onCancel={() => {
+          this.handleCancel();
+        }}
+        customHeader={
+          <DatePickerCustomHeader
+            startDate={this.state.startDate ? this.state.startDate : new Date()}
+            endDate={this.state.endDate ? this.state.endDate : new Date()}
+            selected={this.state.selectedDate}
+          />
+        }
         confirmText={"Ferdig"}
         cancelText={"Avbryt"}
         theme={"dark"}
-        dateConfig={dateConfig}/>);
-
+        dateConfig={dateConfig}
+      />
+    );
   }
 
-  render(){
-    let dateConfig={
-      'year': {
-        format: 'YYYY',
-        caption: 'År',
-        step: 1,
+  render() {
+    let dateConfig = {
+      year: {
+        format: "YYYY",
+        caption: "År",
+        step: 1
       },
-      'month': {
+      month: {
         format: value => monthMap[value.getMonth() + 1],
-        caption: 'Måned',
-        step: 1,
+        caption: "Måned",
+        step: 1
       },
-      'date': {
-        format: 'D',
-        caption: 'Dag',
-        step: 1,
+      date: {
+        format: "D",
+        caption: "Dag",
+        step: 1
       },
-      'hour': {
-        format: 'hh:mm',
-        caption: 'Time',
-        step: 1,
+      hour: {
+        format: "hh:mm",
+        caption: "Time",
+        step: 1
       },
-      'minute': {
-        format: 'mm',
-        caption: 'Min',
-        step: 5,
-      },
+      minute: {
+        format: "mm",
+        caption: "Min",
+        step: 5
+      }
     };
 
-
-    return(
-<div>
-  <Button className="styledPageButton" onClick={() => {this.handleClick()}} style={{
-    maxWidth:"1000px"}}><span>{this.state.startDate ? dateFormatFromAndTo(this.state.startDate,this.state.endDate) : "Dato"}</span></Button>
-  {this.renderFromDate(dateConfig)}
-  {this.renderToDate(dateConfig)}
-</div>
-
+    return (
+      <div>
+        <Button
+          className="styledPageButton"
+          onClick={() => {
+            this.handleClick();
+          }}
+          style={{
+            maxWidth: "1000px"
+          }}
+        >
+          <span>
+            {this.state.startDate
+              ? dateFormatFromAndTo(this.state.startDate, this.state.endDate)
+              : "Dato"}
+          </span>
+        </Button>
+        {this.renderFromDate(dateConfig)}
+        {this.renderToDate(dateConfig)}
+      </div>
     );
   }
 }
--- src/pages/employee.jsx
+++ src/pages/employee.jsx
@@ -1,48 +1,26 @@
-import React, { Component } from 'react';
-import DatePickerCustomHeader from '../components/shared/DatePicker/DatePickerCustomHeader'
+import React, { Component } from "react";
+import DatePickerCustomHeader from "../components/shared/DatePicker/DatePickerCustomHeader";
 
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 //import components
-import EmployeeHeader from '../components/EmployeePage/EmployeeHeader/EmployeeHeader.jsx';
-import Button from '../components/shared/Button/StyledButton';
-import InformationBox from '../components/shared/InformationBox/InformationBox';
-import DatePicker from '../components/shared/DatePicker/DatePicker';
-import Header from '../components/Header/Header.jsx'
+import EmployeeHeader from "../components/EmployeePage/EmployeeHeader/EmployeeHeader.jsx";
+import Button from "../components/shared/Button/StyledButton";
+import InformationBox from "../components/shared/InformationBox/InformationBox";
+import DatePicker from "../components/shared/DatePicker/DatePicker";
+import Header from "../components/Header/Header.jsx";
 
 //import picture
-import TempPic from '../../assets-src/EmployeePage/temp2.png';
+import TempPic from "../../assets-src/EmployeePage/temp2.png";
 
 //import styling
-import '../css/employee.css';
-import '../css/toolbar.css';
+import "../css/employee.css";
+import "../css/toolbar.css";
 
 //import components from framework7
-import {
-  Page,
-  Row,
-  Col,
-  Toolbar,
-  Link
-} from 'framework7-react';
-/*
-const monthMap = {
-  '1': 'Jan',
-  '2': 'Feb',
-  '3': 'Mar',
-  '4': 'Apr',
-  '5': 'May',
-  '6': 'Jun',
-  '7': 'Jul',
-  '8': 'Aug',
-  '9': 'Sep',
-  '10': 'Oct',
-  '11': 'Nov',
-  '12': 'Dec',
-};*/
-
+import { Page, Row, Col, Toolbar, Link } from "framework7-react";
 
 class Employee extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       pic: TempPic,
@@ -54,64 +32,68 @@ class Employee extends Component {
     };
   }
 
-
-/* <a
-            className="select-btn"
-            onClick={this.handleClick.bind(this)}>
-          select timecustomHeader{<button>Test</button>}
-        </a>
-min={this.state.selectedDate === 2 ? this.state.startDate : new Date(1970, 0, 1)}
-
-         */
-
-  dateOnChange(d1, d2){
-    this.setState({startDate:d1, endDate:d2});
+  dateOnChange(d1, d2) {
+    this.setState({ startDate: d1, endDate: d2 });
   }
-
-
 
   render() {
     return (
-    <Page name="home" className="employeePage">
-    <Header backLink title="Employee"/>
-    <div>
-    <EmployeeHeader
-          pic={this.state.pic}
-          position={this.state.position}
-          name={this.state.name}
-        />
-    </div>
-    <Row className="employeePageButtonContainer">
-      <Button>SE LOGG</Button>
-      <Button clicked={ () => this.setState({showDate: !this.state.showDate})}>FORESPØR</Button>
-    </Row>
+      <Page name="home" className="employeePage">
+        <Header backLink title="Employee" />
+        <div>
+          <EmployeeHeader
+            pic={this.state.pic}
+            position={this.state.position}
+            name={this.state.name}
+          />
+        </div>
+        <Row className="employeePageButtonContainer">
+          <Button>SE LOGG</Button>
+          <Button
+            clicked={() => this.setState({ showDate: !this.state.showDate })}
+          >
+            FORESPØR
+          </Button>
+        </Row>
 
-      {this.state.showDate ?
+        {this.state.showDate ? (
+          <Row className="dateButtonContainer margin">
+            <h2>Velg dato:</h2>
+            <DatePicker
+              onChange={(d1, d2) => {
+                this.dateOnChange(d1, d2);
+              }}
+            />
+          </Row>
+        ) : (
+          <div />
+        )}
 
-      <Row className="dateButtonContainer margin">
-        <h2>Velg dato:</h2>
-        <DatePicker onChange={(d1,d2)=>{this.dateOnChange(d1,d2)}}/>
-       </Row>
-          : <div></div>
-
-      }
-
-
-      <Row>
-
-
-
-      </Row>
-    <InformationBox>
-      Lorem Ipsum
-    </InformationBox>
-    <Toolbar className="bottomToolbar" tabbar labels bottom>
-      <Link className="bottomToolbarLink toolbarIcon" href="/" iconF7="home" />
-      <Link className="bottomToolbarLink toolbarIcon" href="/nettverk/" iconF7="search" />
-      <Link className="bottomToolbarLink toolbarIcon" href="/activities/" iconF7="email" />
-      <Link className="bottomToolbarLink toolbarIcon" href="/profilepage/" iconF7="person_round" />
-    </Toolbar>
-    </Page>
+        <Row />
+        <InformationBox>Lorem Ipsum</InformationBox>
+        <Toolbar className="bottomToolbar" tabbar labels bottom>
+          <Link
+            className="bottomToolbarLink toolbarIcon"
+            href="/"
+            iconF7="home"
+          />
+          <Link
+            className="bottomToolbarLink toolbarIcon"
+            href="/nettverk/"
+            iconF7="search"
+          />
+          <Link
+            className="bottomToolbarLink toolbarIcon"
+            href="/activities/"
+            iconF7="email"
+          />
+          <Link
+            className="bottomToolbarLink toolbarIcon"
+            href="/profilepage/"
+            iconF7="person_round"
+          />
+        </Toolbar>
+      </Page>
     );
   }
 }
