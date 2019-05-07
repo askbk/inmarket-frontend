f1cf6cb0ee08eb61caa0a2bc006f12cb059d56ae
Ask Berstad Kolltveit <askbk@protonmail.com>
2019-05-07 18:29:02 +0200

    Remove comment.

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -21,7 +21,6 @@ const Sidebar = () => (
         <ListItem panelClose="right" link="/form/" title='INNSTILLINGER' media={Settings}/>
           {/* Must implement network tab and insert here */}
         <ListItem panelClose="right" link="/nettverk/" title='NETTVERK' media={Network}/>
-        {/* Must implement saved tab and insert here */}
       </List>
     </Page>
   </Panel>
