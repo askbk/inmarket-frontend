dd1860257abcd8776f091beb912e5d4cabcd919f
Z4F1 <pelle.pastoor217@gmail.com>
2019-05-26 20:47:29 +0200

    Fixed route.js

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,7 +1,6 @@
-
 import HomePage from '../pages/home.jsx';
 import AboutPage from '../pages/about.jsx';
-import SettingsPage from '../pages/settings.jsx';
+import FormPage from '../pages/form.jsx';
 import NetworkPage from '../pages/network.jsx';
 import LoginPage from '../pages/login.jsx';
 import RegisterPage from '../pages/register.jsx';
@@ -11,128 +10,86 @@ import NotFoundPage from '../pages/404.jsx';
 import CompanyPage from '../pages/company.jsx';
 import ConversationsPage from '../pages/conversations.jsx';
 import ActivitiesPage from '../pages/activities.jsx';
-import ProfilePage from '../pages/profilepage.jsx';
 import EmployeePage from '../pages/employee.jsx';
 import ChatPage from '../pages/chatPage.jsx';
+import SearchPage from '../pages/search.jsx';
 import DatePicker from '../components/shared/DatePicker/DatePicker';
 import ActivityForm from '../components/shared/ActivityForm/ActivityForm';
-import FormPage from '../pages/form.jsx';
-
+import Profile from '../pages/profile';
+import SettingsPage from '../pages/settings.jsx';
 var routes = [
-  {
-    path: '/',
-    component: HomePage,
-  },
-  {
-    path: '/employee',
-    component: EmployeePage,
-  },
-  {
-    path: '/REMOVETHISLATER',
-    component: ActivityForm,
-  },
-  {
-    path: '/company',
-    component: CompanyPage,
-  },
-  {
-    path: '/about/',
-    component: AboutPage,
-  },
-  {
-    path: '/settings/',
-    component: SettingsPage,
-  },
-  {
-    path: '/nettverk/',
-    component: NetworkPage
-  },
-  {
-      path: '/logginn/',
-      component: LoginPage
-  },
-  {
-      path: '/registrering/',
-      component: RegisterPage
-  },
-  {
-      path: '/profilepage/',
-      component: ProfilePage
-  },
-  {
-    path: '/dynamic-route/blog/:blogId/post/:postId/',
-    component: DynamicRoutePage,
-  },
-  {
-    path: '/conversations/',
-    component: ConversationsPage,
-  },
-  {
-    path: '/activities/',
-    component: ActivitiesPage,
-  },
-  {
-    path: '/conversations/:chatId',
-    component: ChatPage,
-  },
-  {
-    path: '/form/',
-    component: FormPage,
-  },
-  {
-    path: '/request-and-load/user/:userId/',
-    async: function (routeTo, routeFrom, resolve, reject) {
-      // Router instance
-      var router = this;
-
-      // App instance
-      var app = router.app;
-
-      // Show Preloader
-      app.preloader.show();
-
-      // User ID from request
-      var userId = routeTo.params.userId;
-
-      // Simulate Ajax Request
-      setTimeout(function () {
-        // We got user data from request
-        var user = {
-          firstName: 'Vladimir',
-          lastName: 'Kharlampidi',
-          about: 'Hello, i am creator of Framework7! Hope you like it!',
-          links: [
-            {
-              title: 'Framework7 Website',
-              url: 'http://framework7.io',
-            },
-            {
-              title: 'Framework7 Forum',
-              url: 'http://forum.framework7.io',
-            },
-          ]
-        };
-        // Hide Preloader
-        app.preloader.hide();
-
-        // Resolve route to load page
-        resolve(
-          {
-            component: RequestAndLoad,
-          },
-          {
-            context: {
-              user: user,
-            }
-          }
-        );
-      }, 1000);
+    {
+        path: '/',
+        component: HomePage
     },
-  },
-  {
-    path: '(.*)',
-    component: NotFoundPage,
-  },
+    {
+        path: '/employee',
+        component: EmployeePage
+    },
+    {
+        path: '/REMOVETHISLATER',
+        component: ActivityForm
+    },
+    {
+        path: '/company',
+        component: CompanyPage
+    },
+    {
+        path: '/about/',
+        component: AboutPage
+    },
+    {
+        path: '/form/',
+        component: FormPage
+    },
+    {
+        path: '/nettverk/',
+        component: NetworkPage
+    },
+    {
+        path: '/logginn/',
+        component: LoginPage
+    },
+    {
+        path: '/registrering/',
+        component: RegisterPage
+    },
+    {
+        path: '/profile/:id',
+        component: Profile
+    },
+    {
+        path: '/profile/',
+        component: Profile
+    },
+    {
+        path: '/dynamic-route/blog/:blogId/post/:postId/',
+        component: DynamicRoutePage
+    },
+    {
+        path: '/conversations/',
+        component: ConversationsPage
+    },
+    {
+        path: '/activities/',
+        component: ActivitiesPage
+    },
+    {
+        path: '/conversations/:chatId',
+        component: ChatPage
+    },
+    {
+        path: '/sok/',
+        component: SearchPage
+    },
+    {
+        path: '/settings/',
+        component: SettingsPage
+    },
+    {
+        path: '(.*)',
+        component: NotFoundPage
+    }
 ];
 
 export default routes;
