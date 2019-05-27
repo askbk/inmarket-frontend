765bcd78835ea31147785afc3c289f285169175d
Marcus <31349528+Marcusntnu@users.noreply.github.com>
2019-05-27 12:40:31 +0200

    Comment out log

--- src/components/RegisterPage/BasisInformationInput/BasisInformationInput.jsx
+++ src/components/RegisterPage/BasisInformationInput/BasisInformationInput.jsx
@@ -8,7 +8,7 @@ export default class extends React.Component {
     }
 
     render() {
-        console.log(this.props);
+        // console.log(this.props);
         return (
             <React.Fragment>
                 <List>
