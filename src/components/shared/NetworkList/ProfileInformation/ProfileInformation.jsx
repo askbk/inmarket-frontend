d08b5f2d2cde2a47a33630b6f0950fe2faab5d99
Filip Tangen <filiptangen1998@live.com>
2019-05-28 20:07:36 +0200

    Made the search results on search clickable to their profiles

--- src/components/shared/NetworkList/ProfileInformation/ProfileInformation.jsx
+++ src/components/shared/NetworkList/ProfileInformation/ProfileInformation.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './ProfileInformation.css';
 
+import { Link } from 'framework7-react';
+
 //import picture
 import NetworkAvatar from '../NetworkAvatar/NetworkAvatar.jsx';
 import ProfileTextInformation from '../ProfileTextInformation/ProfileTextInformation.jsx';
@@ -12,19 +14,21 @@ export default class extends React.Component {
 
     render() {
         return (
-            <div className='profileInformationContainer'>
-                <NetworkAvatar
-                    profilePic={this.props.profilePic}
-                    status={this.props.connectionStatus}
-                />
-                <ProfileTextInformation
-                    name={this.props.name}
-                    birth={this.props.birth}
-                    role={this.props.role}
-                    location={this.props.location}
-                    rating={this.props.rating}
-                />
-            </div>
+            <Link href={'/profile/' + this.props.userId} style={{height:"100%"}}>
+                <div className='profileInformationContainer'>
+                    <NetworkAvatar
+                        profilePic={this.props.profilePic}
+                        status={this.props.connectionStatus}
+                    />
+                    <ProfileTextInformation
+                        name={this.props.name}
+                        birth={this.props.birth}
+                        role={this.props.role}
+                        location={this.props.location}
+                        rating={this.props.rating}
+                    />
+                </div>
+            </Link>
         );
     }
 }
