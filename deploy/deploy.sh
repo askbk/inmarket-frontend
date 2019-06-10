3733e74d4285d10c340f4323b5ca05851524b24b
ask <askbk@protonmail.com>
2019-06-10 10:34:22 +0200

    Comment out stuff

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,16 +5,16 @@
 scp -i ~/.ssh/id_dsa -r ./www/ ec2-user@172.32.112.3:/tmp
 
 #connect to the server and execute the deployment script
-ssh -i ~/.ssh/id_dsa ec2-user@172.32.112.3
+#ssh -i ~/.ssh/id_dsa ec2-user@172.32.112.3
 
 #first copy the current war to a backup directory (additionaly, I have a cron task deleting old undeployed apps)
-cp -rf app ~/undeployed/apps/;  
+#cp -rf app ~/undeployed/apps/;  
 
 #delete current app
-rm -rf app/nginx/www
+#rm -rf app/nginx/www
 
 #copy the uploaded www
-cp -r /tmp/www app/nginx/www  
+#cp -r /tmp/www app/nginx/www  
 
 #restart webserver
-docker restart production_nginx_1
+#docker restart production_nginx_1
